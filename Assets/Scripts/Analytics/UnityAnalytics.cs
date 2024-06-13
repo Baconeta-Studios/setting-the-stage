5afee25052f7a60304d8c46501745e597fd220e1
James Coburn <61681076+JamesCoburn246@users.noreply.github.com>
2024-06-13 18:39:20 +1200

    Start fleshing out function stubs

--- Assets/Scripts/Analytics/UnityAnalytics.cs
+++ Assets/Scripts/Analytics/UnityAnalytics.cs
@@ -6,6 +6,12 @@ namespace Analytics
 {
     public class UnityAnalytics : EverlastingSingleton<UnityAnalytics>
     {
+        private bool analytics_enabled = false;
+
+        private int total_levels_played;
+        private IDirectionary<string, int> level_play_count; 
+        private int interactions_made_this_attempt;
+
         private async void Start()
         {
             try
@@ -20,23 +26,51 @@ namespace Analytics
 
         private void EnableAnalytics()
         {
+            if (analytics_enabled) return;
+
+            analytics_enabled = true;
         }
 
 
         private void InitializeDefaults()
         {
+            total_levels_played = 0;
+            level_play_count = new IDirectionary<string, int>();
+            interactions_made_this_attempt = 0;            
         }
 
         public void OnLevelStartedEvent()
         {
+            if (level_play_count.ContainsKey(level_id)) {
+                level_play_count[level_id]++;
+            } else {
+                level_play_count.Add(level_id, )
+            }
+
+            if (analytics_enabled) 
+            {
+                InvokeAnalyticsUpdate()
+            }
         }
 
         public void OnStagePlacementEvent()
         {
+
+
+            if (analytics_enabled) 
+            {
+                InvokeAnalyticsUpdate()
+            }
         }
 
         public void OnLevelCompletedEvent()
         {
+
+            
+            if (analytics_enabled) 
+            {
+                InvokeAnalyticsUpdate()
+            }
         }
 
         public void OnLevelAbandonedEvent(
@@ -44,6 +78,16 @@ namespace Analytics
             string act_id,
         )
         {
+            int total_levels_played;
+            int times_played_before;
+            int interactions_made_this_attempt;
+            int score;
+
+
+            
+            if (analytics_enabled) 
+            {
+                InvokeAnalyticsUpdate()
             }
         }
     }
