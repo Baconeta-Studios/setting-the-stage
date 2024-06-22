2462566183caf8eddd2f59b05779243d37d59f5b
Joshua Villgren <joshuavillgren@gmail.com>
2024-06-22 19:02:11 +1200

    Update InstrumentSocket.cs

--- Assets/Scripts/Animation/InstrumentSocket.cs
+++ Assets/Scripts/Animation/InstrumentSocket.cs
@@ -11,17 +11,20 @@ namespace Animation
 
     public class InstrumentSocket : MonoBehaviour
     {
-        // We use RectTransform so we can mark the socket sprites as UI or foreground elements
+        // We use RectTransform for now, so that we can mark the socket sprites as UI or foreground elements
         [SerializeField] private RectTransform leftHandSocket;
         [SerializeField] private RectTransform rightHandSocket;
         [SerializeField] private SpriteRenderer leftHandSprite;
         [SerializeField] private SpriteRenderer rightHandSprite;
 
+        // Here we pass in values that the instrument prefabs will hold telling a player how their socket
+        // must be rotated to support the specific instrument. We elsewhere call the AnimController to tell
+        // the musician which animation they must use in order to hold said instrument in the first place
         public void HoldInstrument(Sprite instrument, Transform instrumentTransform, SocketPosition socket)
         {
             if (instrument == null)
             {
-                StSDebug.LogWarning("Instument sprite null or invalid.");
+                StSDebug.LogWarning("Instrument sprite null or invalid.");
                 return;
             }
 
