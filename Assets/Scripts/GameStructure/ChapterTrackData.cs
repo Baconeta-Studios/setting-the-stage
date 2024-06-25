ab69dcf93547073c2ac00fc331eb14db5ea27723
Joshua Villgren <joshuavillgren@gmail.com>
2024-06-25 23:42:15 +1200

    add instrument choice into scoring system

--- Assets/Scripts/GameStructure/ChapterTrackData.cs
+++ Assets/Scripts/GameStructure/ChapterTrackData.cs
@@ -1,3 +1,5 @@
+using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 
 namespace GameStructure
@@ -10,5 +12,27 @@ namespace GameStructure
         public string chapterTitle;
         public string composerName;
         public string chapterInfo;
+
+        public List<Instrument> correctInstruments;
+        public List<Instrument> veryBadInstruments;
+
+        /// <summary>
+        /// Returns a score between -5 and 5 based on whether this instrument belongs in this track or not.
+        /// </summary>
+        /// <param name="instrument"></param>
+        /// <returns></returns>
+        public int GetInstrumentScore(Instrument instrument)
+        {
+            if (correctInstruments.Any(instrument.Equals))
+            {
+                return 5;
+            }
+            if (veryBadInstruments.Any(instrument.Equals))
+            {
+                return -5;
+            }
+
+            return 0;
+        }
     }
 }
--- Assets/Scripts/Managers/PerformanceController.cs
+++ Assets/Scripts/Managers/PerformanceController.cs
@@ -8,6 +8,11 @@ using UnityEngine;
 public class PerformanceController : MonoBehaviour
 {
     [SerializeField] private bool isPerforming;
+    
+    [Header("Weightings for this chapter")]
+    [SerializeField] [Range(0,100)] private float proficiencyWeightPercent = 50f;
+    [SerializeField] [Range(0,100)] private float instrumentWeightPercent = 50f;
+
     private AudioBuilderSystem _audioBuilderSystem;
 
     public static event Action OnPerformanceStarted; 
@@ -32,6 +37,11 @@ public class PerformanceController : MonoBehaviour
         if (_audioBuilderSystem is null)
         {
             StSDebug.LogError("Performance Controller can't find an audioBuilderSystem in the scene!");
+        }
+
+        if (proficiencyWeightPercent + instrumentWeightPercent != 100)
+        {
+            StSDebug.LogWarning("The weightings in this chapter do not sum to 100 and may cause issues with scoring.");
         }
     }
 
@@ -88,6 +98,48 @@ public class PerformanceController : MonoBehaviour
 
     private float CalculateStars()
     {
+        float proficiencyStars = GetProficiencyStars();
+        float instrumentStars = GetInstrumentStars();
+
+        float weightedAverage = proficiencyStars * (proficiencyWeightPercent / 100) +
+                                instrumentStars * (instrumentWeightPercent / 100);
+        
+        // Double then floor to the nearest integer
+        weightedAverage = Mathf.Floor(weightedAverage * 2);
+
+        // Half to get the average proficiency rounded DOWN to the nearest 0.0f or 0.5f
+        weightedAverage /= 2;
+
+        StSDebug.Log($"Performance Complete, earned {weightedAverage} Stars");
+
+        return weightedAverage;
+    }
+
+    private static float GetInstrumentStars()
+    {
+        int talliedInstrumentChoices = 0;
+        
+        List<StagePosition> stagePositions = StageSelection.Instance.GetStagePositions();
+        Chapter chapter = FindObjectOfType<Chapter>();
+        if (chapter == null)
+        {
+            StSDebug.LogError("Chapter object missing from scene.");
+            return 0;
+        }
+        
+        foreach (StagePosition stagePosition in stagePositions)
+        {
+            talliedInstrumentChoices += chapter.chapterTrackData.GetInstrumentScore(stagePosition.instrumentOccupied);
+        }
+
+        // Get the average proficiency
+        float stars = (float)talliedInstrumentChoices / stagePositions.Count;
+
+        return stars;
+    }
+
+    private static float GetProficiencyStars()
+    {
         int talliedProficiency = 0;
         
         List<StagePosition> stagePositions = StageSelection.Instance.GetStagePositions();
@@ -99,15 +151,6 @@ public class PerformanceController : MonoBehaviour
 
         // Get the average proficiency
         float stars = (float)talliedProficiency / stagePositions.Count;
-
-        // Double then floor to the nearest integer
-        stars = Mathf.Floor(stars * 2);
-
-        // Half to get the average proficiency rounded DOWN to the nearest 0.0f or 0.5f
-        stars /= 2;
-        
-        StSDebug.Log($"Performance Complete, earned {stars} Stars");
-
         return stars;
     }
 }
