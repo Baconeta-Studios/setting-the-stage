2cdedbe9e48d31bf813abba176f658afcbc3fcf0
James Coburn <61681076+JamesCoburn246@users.noreply.github.com>
2024-06-26 19:44:36 +1200

    Show exit button if we aren't on mobile/webgl

--- Assets/Scripts/Utils/ExitGameHelper.cs
+++ Assets/Scripts/Utils/ExitGameHelper.cs
@@ -3,6 +3,19 @@ using Utils;
 
 public class ExitGameHelper : EverlastingSingleton<ExitGameHelper>
 {
+    [SerializeField] private GameObject _exitButton;
+
+    public void OnEnable()
+    {
+        // The exit button will be hidden on mobile and WebGL platforms.
+        if (UnityHelper.IsMobile || UnityHelper.IsWebGL)
+        {
+            _exitButton.SetActive(false);
+        } else {
+            _exitButton.SetActive(true);
+        }
+    }
+
     public void ExitGame()
     {
         Application.Quit();
