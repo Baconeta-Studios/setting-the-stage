fcb6248bd15396ae06abe2918bb966a2c0da04df
James Coburn <61681076+JamesCoburn246@users.noreply.github.com>
2025-09-15 02:37:50 +1200

    Adjust post-processing to be more comprehensive

--- Assets/Editor/XCodePostProcessor.cs
+++ Assets/Editor/XCodePostProcessor.cs
@@ -48,11 +48,20 @@ public class XCodePostProcessor : MonoBehaviour
         Debug.Log($"Using code signing identity '{codeSignIdentity}'.");
 
         string iPhoneUnityTarget = project.GetUnityMainTargetGuid();
+        string unityFramework = project.GetUnityFrameworkTargetGuid();
         string releaseConfigGuid = project.BuildConfigByName(iPhoneUnityTarget, (devMode ? "Debug" : "Release"));
 
         // Set CODE_SIGN_IDENTITY at target-level AND at release-level.
         project.SetBuildProperty(iPhoneUnityTarget, "CODE_SIGN_IDENTITY", codeSignIdentity);
+        project.SetBuildProperty(unityFramework, "CODE_SIGN_IDENTITY", codeSignIdentity);
         project.SetBuildPropertyForConfig(releaseConfigGuid, "CODE_SIGN_IDENTITY", codeSignIdentity);
+
+        // Set DEVELOPMENT_TEAM at target-level AND at release-level.
+        project.SetBuildProperty(iPhoneUnityTarget, "DEVELOPMENT_TEAM", devTeam);
+        project.SetBuildProperty(unityFramework, "DEVELOPMENT_TEAM", devTeam);
+        project.SetBuildPropertyForConfig(releaseConfigGuid, "DEVELOPMENT_TEAM", devTeam);
+
+        // Set CODE_SIGN_STYLE and PROVISIONING_PROFILE_SPECIFIER at the release-level ONLY.
         if (useAutomaticSigning)
         {
             // Configure automatic signing.
@@ -65,7 +74,6 @@ public class XCodePostProcessor : MonoBehaviour
             project.SetBuildPropertyForConfig(releaseConfigGuid, "CODE_SIGN_STYLE", "Manual");
             project.SetBuildPropertyForConfig(releaseConfigGuid, "PROVISIONING_PROFILE_SPECIFIER", provisioningProfile);
         }
-        project.SetBuildPropertyForConfig(releaseConfigGuid, "DEVELOPMENT_TEAM", devTeam);
 
         // Save changes.
         project.WriteToFile(projectPath);
