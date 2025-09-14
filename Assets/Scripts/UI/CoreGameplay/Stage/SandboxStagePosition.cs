5f4a3afd5e67d3a42105f3b224697c73ddbdb199
Josh Villgren <joshuavillgren@gmail.com>
2025-09-14 23:32:55 +1200

    comments

--- Assets/Scripts/UI/CoreGameplay/Stage/SandboxStagePosition.cs
+++ Assets/Scripts/UI/CoreGameplay/Stage/SandboxStagePosition.cs
@@ -1,5 +1,7 @@
 ﻿using Audio;
 
+/// This will allow us to have special logic like selecting a music track from the
+/// UI and telling the audio manager which track we selected to add it to the audiobuilder <inheritdoc />
 public class SandboxStagePosition : StagePosition
 {
     public SandboxAudioDataManager sandboxAudioDataManager;
