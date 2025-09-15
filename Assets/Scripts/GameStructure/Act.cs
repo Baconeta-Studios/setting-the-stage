a7d6a8b2a4e7bb301b4de3948dc0167a4e35804a
Josh Villgren <joshuavillgren@gmail.com>
2025-09-16 00:27:31 +1200

    Update Act.cs

--- Assets/Scripts/GameStructure/Act.cs
+++ Assets/Scripts/GameStructure/Act.cs
@@ -213,6 +213,12 @@ public class Act : MonoBehaviour
 
         if (CheckIfActIsComplete())
         {
+            if (GameDebugManager.Instance.isDemo)
+            {
+                // Never skip to next Act in demo mode
+                return;
+            }
+            
             ProgressToNextAct();
         }
     }
@@ -306,12 +312,6 @@ public class Act : MonoBehaviour
 
     private bool CheckIfActIsComplete()
     {
-        if (GameDebugManager.Instance.isDemo)
-        {
-            // Never skip to next Act in demo mode
-            return false;
-        }
-
         foreach (ChapterStruct chapter in chapters)
         {
             if (chapter.starsEarned <= 0.0f)
