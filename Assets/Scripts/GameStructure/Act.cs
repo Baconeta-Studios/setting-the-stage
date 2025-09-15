9a5366c8e343ebfd118f795f241af44a0ba8bfd8
Josh Villgren <joshuavillgren@gmail.com>
2025-09-16 00:04:45 +1200

    remove auto next act logic

--- Assets/Scripts/GameStructure/Act.cs
+++ Assets/Scripts/GameStructure/Act.cs
@@ -306,6 +306,12 @@ public class Act : MonoBehaviour
 
     private bool CheckIfActIsComplete()
     {
+        if (GameDebugManager.Instance.isDemo)
+        {
+            // Never skip to next Act in demo mode
+            return false;
+        }
+
         foreach (ChapterStruct chapter in chapters)
         {
             if (chapter.starsEarned <= 0.0f)
