dd656a631f96fbfd44b2ef9b0210d2852ce93319
Josh Villgren <joshuavillgren@gmail.com>
2025-09-16 00:03:46 +1200

    add debug manager

---
+++ Assets/Scripts/Managers/GameDebugManager.cs
@@ -0,0 +1,10 @@
+﻿using Utils;
+
+namespace Managers
+{
+    public class GameDebugManager : Singleton<GameDebugManager>
+    {
+        public bool isDemo = false;
+        public bool cheatsEnabled;
+    }
+}
