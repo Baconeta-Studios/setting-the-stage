d70f96103f4be82d6ca126d70de8693ddad1705b
Josh Villgren <joshuavillgren@gmail.com>
2025-09-24 21:42:21 +1200

    fix bg audio not stopping when entering sandbox mode

---
+++ Assets/Scripts/Audio/MenuAudioPlayer.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+
+namespace Audio
+{
+    public class MenuAudioPlayer : MonoBehaviour
+    {
+        public void StopMenuAudio()
+        {
+            MainMenuAudio.Instance.StopMenuAudio();
+        }
+
+        public void RestartMenuAudio()
+        {
+            MainMenuAudio.Instance.RestartMenuAudio();
+        }
+
+        private void StartBackgroundMusic()
+        {
+            MainMenuAudio.Instance.StartBackgroundMusic();
+        }
+    }
+}
